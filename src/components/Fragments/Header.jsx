b9dfc217f14ab78d431dd105eb0a3ae3818a7925
jesikatrinitaa <111202214257@mhs.dinus.ac.id>
2024-11-04 17:00:22 +0700

    coba di lokal baru

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,6 +1,4 @@
-import Input from "../Elements/Labelinput/Input";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
+import Input from "../Elements/LabeledInput/Input";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Header = () => {
@@ -10,14 +8,13 @@ const Header = () => {
     <header className="border-b-2 py-4 px-6 flex items-center justify-between">
       <div className="flex">
         <div className="font-bold text-lg">Username</div>
-        <div className="ms-6 text-gray-500">{date}</div>
+        <div className="ms-6 text-gray-03">{date}</div>
       </div>
       <div className="flex">
         <div className="self-center">
-          <FontAwesomeIcon icon={faBell} className="text-gray-500 scale-110" />
+          <NotificationsIcon className="text-gray-01 scale-110" />
         </div>
         <div className="ms-10 hidden sm:block">
-          <input variant="bg-special-mainBg border border-gray-03" />,
           <Input variant="bg-white w-80" />
         </div>
       </div>
